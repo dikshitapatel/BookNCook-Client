10c94b8599a930ea1788a627c5fa50403f41d906
Karan Patel <shivanipatel327@gmail.com>
2018-01-20 09:11:08 +0530

    commited

--- app/src/main/java/com/example/atul_/eatit/Home.java
+++ app/src/main/java/com/example/atul_/eatit/Home.java
@@ -1,25 +1,23 @@
 package com.example.atul_.eatit;
 
 import android.content.Intent;
-import android.icu.util.ULocale;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.atul_.eatit.Common.Common;
 import com.example.atul_.eatit.Interface.ItemClickListener;
@@ -37,17 +35,18 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     RecyclerView recycler_menu;
     RecyclerView.LayoutManager layoutManager;
 
-    final FirebaseRecyclerAdapter<Category,MenuViewHolder> adapter;
+    //final FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Menu");
         setSupportActionBar(toolbar);
 
-        database=FirebaseDatabase.getInstance();
-        category=database.getReference("Category");
+        database = FirebaseDatabase.getInstance();
+        category = database.getReference("Category");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,15 +66,16 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        View headerView=navigationView.getHeaderView(0);
+        View headerView = navigationView.getHeaderView(0);
         txtFullName=(TextView)headerView.findViewById(R.id.txtFullName);
         txtFullName.setText(Common.currentUser.getName());
         recycler_menu=(RecyclerView)findViewById(R.id.recycler_menu);
         recycler_menu.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         recycler_menu.setLayoutManager(layoutManager);
         loadMenu();
     }
+
     private void loadMenu() {
         final FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>() {
             protected void populateViewHolder(MenuViewHolder viewHolder, Category model, int position) {
@@ -95,8 +95,6 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
             }
 
 
-            @RequiresApi(api = Build.VERSION_CODES.GINGERBREAD)
-            @Override
 
             public void onBackPressed() {
                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -107,27 +105,25 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 }
             }
 
-            @Override
+
             public boolean onCreateOptionsMenu(Menu menu) {
                 // Inflate the menu; this adds items to the action bar if it is present.
                 getMenuInflater().inflate(R.menu.home, menu);
                 return true;
             }
 
-            @Override
             public boolean onOptionsItemSelected(MenuItem item) {
 
 
                 return super.onOptionsItemSelected(item);
             }
 
-            @SuppressWarnings("StatementWithEmptyBody")
-            @Override
+           // @SuppressWarnings("StatementWithEmptyBody")
+
             public boolean onNavigationItemSelected(MenuItem item) {
                 // Handle navigation view item clicks here.
                 int id = item.getItemId();
-
-                if (id == R.id.nav_menu) {
+                 if (id == R.id.nav_menu) {
                     // Handle the camera action
                 } else if (id == R.id.nav_cart) {
 
